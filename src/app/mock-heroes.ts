a0789a483345c8f0114b3ecab074e329de87ae4f
danrfiuza <danielfiuza01@gmail.com>
2018-05-02 20:20:56 -0300

    second commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Ant Man' },
+    { id: 12, name: 'Iron Man' },
+    { id: 13, name: 'Thor' },
+    { id: 14, name: 'Lord Star' },
+    { id: 15, name: 'Vision' },
+    { id: 16, name: 'Black Panther' },
+    { id: 17, name: 'Spider Man' },
+    { id: 18, name: 'black Widow' },
+    { id: 19, name: 'Archer' },
+    { id: 20, name: 'Iron Patriot' }
+];
